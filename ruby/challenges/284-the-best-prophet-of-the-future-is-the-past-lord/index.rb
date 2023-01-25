9a4234ef6bfbb74e4f59872bf3b1c5e885a3467e
Ángel Guerra <a.guerrarevilla@gmail.com>
2023-01-25 07:55:23 +0100

    Solves challenge 284 with Ruby

---
+++ ruby/challenges/284-the-best-prophet-of-the-future-is-the-past-lord/index.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# **You are given a list of positive integers which represents some range of integers which has been truncated. Find
+# the missing bits, insert ellipses to show that that part has been truncated, and print it.** If the consecutive
+# values differ by exactly two, then insert the missing value.
+#
+# Example
+# ```js
+# missingBits([1, 2, 3, 4, 20, 21, 22, 23]); // "[1,2,3,4,...,20,21,22,23]"
+# missingBits([1, 2, 3, 5, 6]); // "[1,2,3,4,5,6]"
+# missingBits([1, 3, 20, 27]); // "[1,2,3,...,20,...,27]"
+# ```
+module MissingBits
+  RANGE_FILL = "..."
+
+  def self.run(bits, gap = 2)
+    result = bits.sort.inject([]) { |acc, current| reducer(acc, current, gap) }
+
+    "[#{result.join(", ")}]"
+  end
+
+  def self.reducer(acc, current, gap)
+    prev = acc.last
+
+    return [*acc, current] if prev.nil? || current - prev == 1
+
+    return [*acc, RANGE_FILL, current] if current - prev > gap
+
+    acc + gap.times.reverse_each.map { |idx| current - idx }
+  end
+end
